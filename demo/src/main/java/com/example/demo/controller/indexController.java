c28149eca6d3cc13d9dde44a36d60c7e7123b2d3
Bisucit <biscuitv@163.com>
2022-10-14 10:49:26 +0800

    Initial commit

---
+++ demo/src/main/java/com/example/demo/controller/indexController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class indexController {
+
+    @RequestMapping("/index")
+    public String index(){
+        System.out.println("indexController");
+        return "indexController";
+    }
+
+}
